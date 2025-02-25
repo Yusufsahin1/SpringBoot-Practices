918df6c3995f0366b59253bd6285b5b0a720ae0b
Yusuf Şahin <99734246+Yusufsahin1@users.noreply.github.com>
2025-02-25 17:42:37 +0300

    Add files via upload

---
+++ 04-spring-security-in-memory/src/test/java/com/yusufsahin/spring_security_in_memory/SpringSecurityInMemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yusufsahin.spring_security_in_memory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityInMemoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
